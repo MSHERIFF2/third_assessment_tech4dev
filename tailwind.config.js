2966dcb58c2ed37efe9826041f1eb068193d5594
Sheriffdeen Muritala Adegoke <muritalasheriffdeenadegoke@gmail.com>
2024-12-11 16:35:23 +0000

    updaated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/*.[html, css, js"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
